fa49e2f04b6c494d64280473b0b5a1c0ec0f5292
YashrajTheDev <128779095+YashrajTheDev@users.noreply.github.com>
2024-10-25 09:00:51 +0530

    Create Admin.js

---
+++ models/Admin.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const AdminSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+AdminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
